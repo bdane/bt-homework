b0a74714cddeec382481cc9e4f6a48ee429b331d
bdane <bdaner@gmail.com>
2022-05-08 21:33:09 +0200

    nesto mi izmenilo kod

--- hw11/register.php
+++ hw11/register.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 
@@ -81,48 +80,4 @@ if (isset($_GET['sub'])) {
 
 </body>
 
-=======
-<?php
-
-$ime = $_GET['ime'];
-$prezime = $_GET['prez'];
-$pol = $_GET['pol'];
-$email = $_GET['email'];
-$pass = $_GET['pass'];
-$re_pass = $_GET['re-pass'];
-$courses = $_GET['courses'];
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-
-<body>
-    <div class="container">
-        <?php
-        if ($pass !== $re_pass) {
-            echo "Ponovljena lozinka mora biti ista kao prethodno uneta lozinka!";
-        } else {
-
-            if ($pol[0] == "m") {
-                echo "Postovani ";
-            } else {
-                echo "Postovana ";
-            }
-        }
-        echo "$ime $prezime, uspesno ste se registrovali na nasem sajtu.<br>";
-        echo "Vasa lozinka je: $pass";
-        ?>
-    </div>
-
-</body>
-
->>>>>>> 1f8462de8c435d329fa22d43739320aea160b12e
 </html>
